dde1d2164a38a2bca6544cb3adbfd76fa0369627
joydeep <joydeepbeyondsky86@gmail.com>
2020-02-10 07:12:07 +0530

    Added CORS. Still not working though

--- Profile/Startup.cs
+++ Profile/Startup.cs
@@ -6,53 +6,70 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Profile.Entityframework.EF;
 
-namespace Profile
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Profile {
+    public class Startup {
+
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers().AddNewtonsoftJson();
-
-            services.AddDbContextPool<CroudPulseDbContext>(opts =>
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors(options =>
             {
-                opts.UseMySql(Configuration.GetConnectionString("CroudPulseDbContext"));
+
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+
+                options.AddPolicy(MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            services.AddControllers ().AddNewtonsoftJson ();
+
+            services.AddDbContextPool<CroudPulseDbContext> (opts => {
+                opts.UseMySql (Configuration.GetConnectionString ("CroudPulseDbContext"));
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+
+
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
-            app.UseRouting();
+            app.UseRouting ();
+            app.UseCors(MyAllowSpecificOrigins);
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ().RequireCors(MyAllowSpecificOrigins); ;
             });
         }
     }

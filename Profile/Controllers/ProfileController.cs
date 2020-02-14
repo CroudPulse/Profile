f96be502c9c408e3d4055fae137fd79f37ad6f38
joydeep <joydeepbeyondsky86@gmail.com>
2020-02-14 14:03:41 +0530

    Added Notification service call

--- Profile/Controllers/ProfileController.cs
+++ Profile/Controllers/ProfileController.cs
@@ -7,44 +7,54 @@ using Profile.Entityframework.EF;
 using Profile.Mock;
 using EF = Profile.Entityframework.EF;
 
-namespace Profile.Controllers {
-    [Route ("api/[controller]")]
+namespace Profile.Controllers
+{
+    [Route("api/[controller]")]
     [ApiController]
-    public class ProfileController : ControllerBase {
+    public class ProfileController : ControllerBase
+    {
         private readonly CroudPulseDbContext context;
+        private readonly INotificationService notify;
 
-        public ProfileController (CroudPulseDbContext context) {
+        public ProfileController(CroudPulseDbContext context,
+            INotificationService notify)
+        {
             this.context = context;
+            this.notify = notify;
         }
 
         // GET: api/Profile
         [HttpGet]
-        public IEnumerable<string> Get () {
+        public IEnumerable<string> Get()
+        {
             return new string[] { "value1", "value2" };
         }
 
         // GET: api/Profile/5
-        [HttpGet ("{id}", Name = "Get")]
-        public string Get (int id) {
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
             return "value";
         }
 
         // POST: api/Profile
         [HttpPost]
-        public async Task<IActionResult> Post ([FromBody] EF.Profile profile) {
+        public async Task<IActionResult> Post([FromBody] EF.Profile profile)
+        {
             // profile.ProfileId = Guid.NewGuid ().ToString ();
-            profile = ProfileMock.People ();
-            this.context.Profile.Add (profile);
-            await this.context.SaveChangesAsync ();
-            return Ok (profile);
+            profile = ProfileMock.People();
+            this.context.Profile.Add(profile);
+            await this.context.SaveChangesAsync();
+            await notify.Notify($"{profile.FirstName} {profile.LastName} got created. ID: {profile.ProfileId}");
+            return Ok(profile);
         }
 
         // PUT: api/Profile/5
-        [HttpPut ("{id}")]
-        public void Put (int id, [FromBody] string value) { }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
 
         // DELETE: api/ApiWithActions/5
-        [HttpDelete ("{id}")]
-        public void Delete (int id) { }
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
     }
 }

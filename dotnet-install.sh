b5c5f23206d53e4c0f4729162f1eba404ea8d338
Joydeep Roychowdhury <jroy@pop-os.localdomain>
2020-03-06 09:10:14 +0530

    Removed sudo Gaaah!!!

--- dotnet-install.sh
+++ dotnet-install.sh
@@ -1,5 +1,5 @@
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
-sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
 wget -q https://packages.microsoft.com/config/debian/10/prod.list
 mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg

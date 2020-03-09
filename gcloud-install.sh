e9e305d4a12979533ad2902195c5adc5b39433dc
joydeep <joydeepbeyondsky86@gmail.com>
2020-03-09 23:12:19 +0530

    try installing only with curl and invoking sh

--- gcloud-install.sh
+++ gcloud-install.sh
@@ -1,8 +1,4 @@
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1; 
-sudo apk add --update \
- python \
- curl \
- which \
- bash
-curl -s https://sdk.cloud.google.com | bash
+apk add --update curl 
+curl -s https://sdk.cloud.google.com | sh
